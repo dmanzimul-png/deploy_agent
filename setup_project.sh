d445c795067c3ab63404b9dd53f144e3799f1b36
Elvis <dmanzimul@alustudent.com>
2026-02-14 18:34:56 +0200

    file created

--- setup_project.sh
+++ setup_project.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+read -p "Enter project name: " project_name
+parent_dir="attendance_tracker_$project_name"
+mkdir -p "$parent_dir/Helpers"
+mkdir -p "$parent_dir/reports"
+touch "$parent_dir/attendance_checker.py"
+touch "$parent_dir/Helpers/assets.csv"
+touch "$parent_dir/Helpers/config.json"
+touch "$parent_dir/reports/reports.log"
+echo "Folders and files created sucessfully"
 
