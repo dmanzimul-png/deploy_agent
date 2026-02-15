689bf6790e32eb722bf885b9b5678ee251e5ca89
Elvis <dmanzimul@alustudent.com>
2026-02-15 14:30:18 +0200

    adding threshold prompts and config.json

--- setup_project.sh
+++ setup_project.sh
@@ -8,4 +8,9 @@ touch "$parent_dir/Helpers/assets.csv"
 touch "$parent_dir/Helpers/config.json"
 touch "$parent_dir/reports/reports.log"
 echo "Folders and files created sucessfully"
+read -p "warning level set [default=75]: " warning 
+warning=${warning:-75}
+read -p "failure level set [default 50]:" failure
+failure=${failure:-50}
+echo "{ \"warning\": $warning, \"failure\": $failure }" > "$parent_dir/Helpers/config.json"
 
